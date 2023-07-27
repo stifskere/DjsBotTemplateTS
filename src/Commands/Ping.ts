9b0f57486958b6b8f4cac7fc14c3df51327929b7
Stif <stifskere@gmail.com>
2023-07-27 10:39:22 +0200

    first commit

---
+++ src/Commands/Ping.ts
@@ -0,0 +1,10 @@
+import {CommandBuilder, CommandHandler} from "../Exports";
+
+export default class Info extends CommandHandler {
+    data: CommandBuilder;
+
+    async execute(): Promise<void> {
+
+    }
+
+}
